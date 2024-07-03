2368f467728bfdcc8e8b215aef3e5863c2281872
justin <justin@jle.im>
2024-07-03 15:16:50 -0700

    add template and refactor flake

--- flake.nix
+++ flake.nix
@@ -11,87 +11,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlay = final: pkgs: {
-          haskell-project-flake = { name, src, defaultCompiler, shellOverrides ? { }, excludeCompilerMajors ? [ ] }:
-            let
-              compilers = pkgs.lib.attrsets.filterAttrs (n: _: !(pkgs.lib.strings.hasSuffix "llvm" n) && !(builtins.elem n excludeCompilerMajors))
-                pkgs.haskell-nix.compilerNameMap // { default = defaultCompiler; };
-              shell =
-                pkgs.lib.attrsets.recursiveUpdate
-                  {
-                    withHoogle = false;
-                    tools = {
-                      cabal = { };
-                      hlint = { };
-                      haskell-language-server = { };
-                      fourmolu = { };
-                    };
-                  }
-                  shellOverrides;
-              projects =
-                builtins.mapAttrs
-                  (n: c:
-                    pkgs.haskell-nix.project' {
-                      inherit name src;
-                      compiler-nix-name = c;
-                      shell = if n == "default" then shell else { };
-                    }
-                  )
-                  compilers;
-              checkFormat = pkgs.runCommandLocal "checkFormat"
-                {
-                  inherit src;
-                  nativeBuildInputs = [ (projects.default.tool "fourmolu" shell.tools.fourmolu) ];
-                } ''
-                cd $src
-                fourmolu --mode check . > $out
-              '';
-              runFormat =
-                pkgs.writeShellApplication {
-                  name = "formatHaskell.sh";
-                  runtimeInputs = [ (projects.default.tool "fourmolu" shell.tools.fourmolu) ];
-                  text = ''
-                    # shellcheck disable=SC2046
-                    fourmolu --mode inplace $(git ls-files '*.hs')
-                  '';
-                };
-              packagesByCompiler =
-                builtins.mapAttrs
-                  (v: project:
-                    project.flake'.packages
-                    // {
-                      all = pkgs.symlinkJoin {
-                        name = "${name}-${v}";
-                        paths = pkgs.lib.mapAttrsToList (_: package: package) projects.default.flake'.packages;
-                      };
-                    }
-                  )
-                  projects;
-            in
-            rec {
-              inherit projects packagesByCompiler;
-              packages = builtins.mapAttrs (_: p: p.all) packagesByCompiler // {
-                default =
-                  packagesByCompiler.default.all;
-                everyCompiler = pkgs.symlinkJoin {
-                  name = "${name}-every-compiler";
-                  paths = pkgs.lib.mapAttrsToList (_: package: package.all) packagesByCompiler;
-                };
-              };
-              apps = {
-                format = {
-                  type = "app";
-                  program = "${runFormat}/bin/formatHaskell.sh";
-                };
-              };
-              devShells = builtins.mapAttrs (_: project: project.flake'.devShells.default) projects;
-              checks = projects.default.flake'.checks // {
-                inherit checkFormat;
-              };
-            };
+          haskell-project-flake = pkgs.callPackage ./default.nix { };
         };
       in
       rec {
-        overlays.default = (import nixpkgs { inherit system; }).lib.fixedPoints.composeExtensions haskellNix.overlay overlay;
+        overlays.default = (import nixpkgs { inherit system; }).lib.fixedPoints.composeExtensions
+          haskellNix.overlay
+          overlay;
+        templates.haskell-project-flake = {
+          path = ./templates/haskell-project-flake;
+          description = "Set up haskell project infrastructure";
+        };
+        templates.default = templates.haskell-project-flake;
         legacyPackages = import nixpkgs {
           inherit system;
           inherit (haskellNix) config;
