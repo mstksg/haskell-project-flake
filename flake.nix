0153552b72c0bb232ef3a029a730dc437504fa68
justin <justin@jle.im>
2024-07-03 15:58:45 -0700

    use template scorrectly

--- flake.nix
+++ flake.nix
@@ -8,26 +8,29 @@
   };
 
   outputs = { self, nixpkgs, haskellNix, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlay = final: pkgs: {
-          haskell-project-flake = pkgs.callPackage ./default.nix { };
-        };
-      in
-      rec {
-        overlays.default = (import nixpkgs { inherit system; }).lib.fixedPoints.composeExtensions
-          haskellNix.overlay
-          overlay;
-        templates.haskell-project-flake = {
-          path = ./templates/haskell-project-flake;
-          description = "Set up haskell project infrastructure";
-        };
-        templates.default = templates.haskell-project-flake;
-        legacyPackages = import nixpkgs {
-          inherit system;
-          inherit (haskellNix) config;
-          overlays = [ overlays.default ];
-        };
-      }
-    );
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlay = final: pkgs: {
+            haskell-project-flake = pkgs.callPackage ./default.nix { };
+          };
+        in
+        rec {
+          overlays.default = (import nixpkgs { inherit system; }).lib.fixedPoints.composeExtensions
+            haskellNix.overlay
+            overlay;
+          legacyPackages = import nixpkgs {
+            inherit system;
+            inherit (haskellNix) config;
+            overlays = [ overlays.default ];
+          };
+        }
+      ) // rec {
+      templates.haskell-project-flake = {
+        path = ./templates/haskell-project-flake;
+        description = "Set up haskell project infrastructure";
+      };
+      templates.default = templates.haskell-project-flake;
+    }
+  ;
 }
